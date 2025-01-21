ace085ac70b282d372610dc2b71d34daba544747
Jschwabe99 <153418411+Jschwabe99@users.noreply.github.com>
2025-01-21 14:34:20 +0900

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
 
     public static void main(String[] args) {
+        // Creates a list of 100 Trials 
         Trial[] simulation = new Trial[100];
         for(int i = 0; i < simulation.length; i++){
             simulation[i] = new Trial();
